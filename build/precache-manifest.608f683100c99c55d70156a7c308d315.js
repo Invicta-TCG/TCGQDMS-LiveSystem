38cb037bdae5311930d07b1bc0de97b0f71b6653
Mathuragini <mathuthiyasharma@gmail.com>
2020-01-24 17:58:45 +0530

    first commir

---
+++ build/precache-manifest.608f683100c99c55d70156a7c308d315.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5206d2d413cce04061607df611b16d",
+    "url": "/TCGQDMS-LiveSystem/index.html"
+  },
+  {
+    "revision": "ce940f2ba1069f8a6e82",
+    "url": "/TCGQDMS-LiveSystem/static/css/2.41e92cc3.chunk.css"
+  },
+  {
+    "revision": "a710e3df2bac44fa876b",
+    "url": "/TCGQDMS-LiveSystem/static/css/main.bd898ac2.chunk.css"
+  },
+  {
+    "revision": "ce940f2ba1069f8a6e82",
+    "url": "/TCGQDMS-LiveSystem/static/js/2.735c216a.chunk.js"
+  },
+  {
+    "revision": "ef03a1b844044b3952465b0816f0481a",
+    "url": "/TCGQDMS-LiveSystem/static/js/2.735c216a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a710e3df2bac44fa876b",
+    "url": "/TCGQDMS-LiveSystem/static/js/main.6fdb8a2b.chunk.js"
+  },
+  {
+    "revision": "fa51387827ddfb7eb116",
+    "url": "/TCGQDMS-LiveSystem/static/js/runtime-main.320e45c5.js"
+  },
+  {
+    "revision": "616f227393973c2e592f651fb68e70e2",
+    "url": "/TCGQDMS-LiveSystem/static/media/accounts.616f2273.png"
+  },
+  {
+    "revision": "2763f87a4fe4141a8965d5a13d92de72",
+    "url": "/TCGQDMS-LiveSystem/static/media/add.2763f87a.png"
+  },
+  {
+    "revision": "6d283536ae6a796f3e7188f8b4809a62",
+    "url": "/TCGQDMS-LiveSystem/static/media/background.6d283536.jpg"
+  },
+  {
+    "revision": "7523e6245eeb31c1c35cb800a757c7c2",
+    "url": "/TCGQDMS-LiveSystem/static/media/cementbag.7523e624.jpg"
+  },
+  {
+    "revision": "b8029dffbceab5c4c701e7ee3b5e470c",
+    "url": "/TCGQDMS-LiveSystem/static/media/concretemixer.b8029dff.jpg"
+  },
+  {
+    "revision": "a563a35f4d15c58966e0deab2bf3709e",
+    "url": "/TCGQDMS-LiveSystem/static/media/configurationedited.a563a35f.png"
+  },
+  {
+    "revision": "5bab6a8f85d0aa4a713d73d075352877",
+    "url": "/TCGQDMS-LiveSystem/static/media/divide.5bab6a8f.png"
+  },
+  {
+    "revision": "7fb0df6e06bd38d94b4fc6285af5d194",
+    "url": "/TCGQDMS-LiveSystem/static/media/equal.7fb0df6e.png"
+  },
+  {
+    "revision": "79c00e3f5f405cc2d0c8ee21926e2d78",
+    "url": "/TCGQDMS-LiveSystem/static/media/excelimport.79c00e3f.png"
+  },
+  {
+    "revision": "0f9dada1fc6def9dcc889e4a3be91c21",
+    "url": "/TCGQDMS-LiveSystem/static/media/finalproduct2.0f9dada1.jpg"
+  },
+  {
+    "revision": "4acad38d2e305ce3393974c9ba5a52c7",
+    "url": "/TCGQDMS-LiveSystem/static/media/homebtn.4acad38d.png"
+  },
+  {
+    "revision": "454b91a3f1fa83dd8f55f82d62583198",
+    "url": "/TCGQDMS-LiveSystem/static/media/images1.454b91a3.jpg"
+  },
+  {
+    "revision": "d8e64872ea248c466e23dae16ebefcf9",
+    "url": "/TCGQDMS-LiveSystem/static/media/is-greater-than.d8e64872.png"
+  },
+  {
+    "revision": "71b10a35ce9867ed9aa4b53c9d25590c",
+    "url": "/TCGQDMS-LiveSystem/static/media/is-less-than.71b10a35.png"
+  },
+  {
+    "revision": "ce0bb88cda53dadd6ecc09cf38aeb5b3",
+    "url": "/TCGQDMS-LiveSystem/static/media/labtesting.ce0bb88c.jpg"
+  },
+  {
+    "revision": "3a676e110b8756d528fc55945b76be9f",
+    "url": "/TCGQDMS-LiveSystem/static/media/logorestablet.3a676e11.png"
+  },
+  {
+    "revision": "3918f0b7c921ac70c3096195ed4ad29c",
+    "url": "/TCGQDMS-LiveSystem/static/media/master.3918f0b7.jpg"
+  },
+  {
+    "revision": "b3b6db522d8baa5ac1efe6ddde26e748",
+    "url": "/TCGQDMS-LiveSystem/static/media/master1.b3b6db52.png"
+  },
+  {
+    "revision": "df48d5c9068f48bee6445e2c25e9bfa8",
+    "url": "/TCGQDMS-LiveSystem/static/media/materialedited.df48d5c9.png"
+  },
+  {
+    "revision": "ae8213159803309e3daccca45f468526",
+    "url": "/TCGQDMS-LiveSystem/static/media/multiply.ae821315.png"
+  },
+  {
+    "revision": "33596a17364b2888b47dce08766c2794",
+    "url": "/TCGQDMS-LiveSystem/static/media/pie.33596a17.png"
+  },
+  {
+    "revision": "571e42d4a5c27238ebf9844df86270df",
+    "url": "/TCGQDMS-LiveSystem/static/media/reports.571e42d4.png"
+  },
+  {
+    "revision": "46111343c05ff14c3e905bac91378c77",
+    "url": "/TCGQDMS-LiveSystem/static/media/settings.46111343.png"
+  },
+  {
+    "revision": "c1a5f58b655d68ba910ede198b88ad0c",
+    "url": "/TCGQDMS-LiveSystem/static/media/square-root.c1a5f58b.png"
+  },
+  {
+    "revision": "692512ca231c4abff8397159528e6863",
+    "url": "/TCGQDMS-LiveSystem/static/media/square.692512ca.png"
+  },
+  {
+    "revision": "6568e29333cb984f510cab8d0d7d648e",
+    "url": "/TCGQDMS-LiveSystem/static/media/subtract.6568e293.png"
+  },
+  {
+    "revision": "b7abbcae2c30d11259afaa30f6e4c4d1",
+    "url": "/TCGQDMS-LiveSystem/static/media/tokyosuper.b7abbcae.png"
+  },
+  {
+    "revision": "ef640527cecadcc53c2e333906bf3c61",
+    "url": "/TCGQDMS-LiveSystem/static/media/zigma.ef640527.png"
+  }
+]);
